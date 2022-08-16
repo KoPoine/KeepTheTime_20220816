5c43695eff4df1067d7b79302d227a58c6d01c4a
넵플러스 <kmc901206@gmail.com>
2022-08-16 12:08:21 +0900

    바텀네이션 메뉴 선택 이벤트 처리

--- app/src/main/java/com/neppplus/keepthetime_20220816/MainActivity.kt
+++ app/src/main/java/com/neppplus/keepthetime_20220816/MainActivity.kt
@@ -19,7 +19,16 @@ class MainActivity : BaseActivity() {
     }
 
     override fun setupEvents() {
-
+//        바텀 네이게이션 메뉴 선택 이벤트 > 뷰페이져의 페이지 이동
+        binding.bottomNav.setOnItemSelectedListener {
+//            어떤 메뉴가 선택되었는가? > it 변수가 알려줌.
+            when (it.itemId) {
+                R.id.myAppointment -> binding.mainViewPager.currentItem = 0
+                R.id.invitedAppointment -> binding.mainViewPager.currentItem = 1
+                R.id.setting -> binding.mainViewPager.currentItem = 2
+            }
+            return@setOnItemSelectedListener true
+        }
     }
 
     override fun setValues() {

d1b9d06a7feea269a4937eb3c5746440380bed91
넵플러스 <kmc901206@gmail.com>
2022-08-16 12:03:57 +0900

    생성한 ViewPagerAdapter를 객체화 및 ViewPager변수와 연결

--- app/src/main/java/com/neppplus/keepthetime_20220816/MainActivity.kt
+++ app/src/main/java/com/neppplus/keepthetime_20220816/MainActivity.kt
@@ -2,10 +2,28 @@ package com.neppplus.keepthetime_20220816
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.neppplus.keepthetime_20220816.adapters.MainViewPagerAdapter
+import com.neppplus.keepthetime_20220816.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity() {
+
+    lateinit var binding : ActivityMainBinding
+    lateinit var mPagerAdapter: MainViewPagerAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        setupEvents()
+        setValues()
+    }
+
+    override fun setupEvents() {
+
+    }
+
+    override fun setValues() {
+        mPagerAdapter = MainViewPagerAdapter(this)
+        binding.mainViewPager.adapter = mPagerAdapter
     }
 }

724b77f1da8df013ac89678b9434404b2fafb004
넵플러스 <kmc901206@gmail.com>
2022-08-16 11:51:28 +0900

    ViewPager에 활용될 Fragment 3개 생성 feat.BaseFrag 생성

---
+++ app/src/main/java/com/neppplus/keepthetime_20220816/fragments/BaseFragment.kt
@@ -0,0 +1,27 @@
+package com.neppplus.keepthetime_20220816.fragments
+
+import android.content.Context
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import com.neppplus.keepthetime_20220816.api.APIList
+import com.neppplus.keepthetime_20220816.api.ServerAPI
+import retrofit2.Retrofit
+
+abstract class BaseFragment : Fragment() {
+
+    lateinit var mContext : Context
+    lateinit var retrofit: Retrofit
+    lateinit var apiList : APIList
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        mContext = requireContext()
+        retrofit = ServerAPI.getRetrofit()
+        apiList = retrofit.create(APIList::class.java)
+    }
+
+    abstract fun setupEvents()
+    abstract fun setValues()
+
+}

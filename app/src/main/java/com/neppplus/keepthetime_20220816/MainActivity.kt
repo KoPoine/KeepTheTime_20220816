8dbaf28905e25da9211dbef41bd957f3bab02926
넵플러스 <kmc901206@gmail.com>
2022-08-16 12:20:29 +0900

    뷰페이저 없이 Fragment Attach 시키는 방법

--- app/src/main/java/com/neppplus/keepthetime_20220816/MainActivity.kt
+++ app/src/main/java/com/neppplus/keepthetime_20220816/MainActivity.kt
@@ -6,6 +6,9 @@ import androidx.databinding.DataBindingUtil
 import androidx.viewpager2.widget.ViewPager2
 import com.neppplus.keepthetime_20220816.adapters.MainViewPagerAdapter
 import com.neppplus.keepthetime_20220816.databinding.ActivityMainBinding
+import com.neppplus.keepthetime_20220816.fragments.InvitedAppointmentFragment
+import com.neppplus.keepthetime_20220816.fragments.MyAppointmentFragment
+import com.neppplus.keepthetime_20220816.fragments.SettingFragment
 
 class MainActivity : BaseActivity() {
 
@@ -24,33 +27,49 @@ class MainActivity : BaseActivity() {
         binding.bottomNav.setOnItemSelectedListener {
 //            어떤 메뉴가 선택되었는가? > it 변수가 알려줌.
             when (it.itemId) {
-                R.id.myAppointment -> binding.mainViewPager.currentItem = 0
-                R.id.invitedAppointment -> binding.mainViewPager.currentItem = 1
-                R.id.setting -> binding.mainViewPager.currentItem = 2
+//                R.id.myAppointment -> binding.mainViewPager.currentItem = 0
+//                R.id.invitedAppointment -> binding.mainViewPager.currentItem = 1
+//                R.id.setting -> binding.mainViewPager.currentItem = 2
+
+                R.id.myAppointment -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.mainViewPager, MyAppointmentFragment()).commit()
+                }
+                R.id.invitedAppointment -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.mainViewPager, InvitedAppointmentFragment()).commit()
+                }
+                R.id.setting -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.mainViewPager, SettingFragment()).commit()
+                }
             }
             return@setOnItemSelectedListener true
         }
 
 //        뷰페이저 페이지 이동 > 바텀 네비게이션의 메뉴 선택
 
-        binding.mainViewPager.registerOnPageChangeCallback(
-            object : ViewPager2.OnPageChangeCallback(){
-//                추상 메쏘드 X. 이벤트 처리 함수를 직접 오버라이딩
-
-                override fun onPageSelected(position: Int) {
-                    super.onPageSelected(position)
-
-                    binding.bottomNav.selectedItemId = when (position) {
-                        0 -> R.id.myAppointment
-                        1 -> R.id.invitedAppointment
-                        else -> R.id.setting
-                    }
-                }
-            })
+//        binding.mainViewPager.registerOnPageChangeCallback(
+//            object : ViewPager2.OnPageChangeCallback(){
+////                추상 메쏘드 X. 이벤트 처리 함수를 직접 오버라이딩
+//
+//                override fun onPageSelected(position: Int) {
+//                    super.onPageSelected(position)
+//
+//                    binding.bottomNav.selectedItemId = when (position) {
+//                        0 -> R.id.myAppointment
+//                        1 -> R.id.invitedAppointment
+//                        else -> R.id.setting
+//                    }
+//                }
+//            })
     }
 
     override fun setValues() {
-        mPagerAdapter = MainViewPagerAdapter(this)
-        binding.mainViewPager.adapter = mPagerAdapter
+//        mPagerAdapter = MainViewPagerAdapter(this)
+//        binding.mainViewPager.adapter = mPagerAdapter
+
+        supportFragmentManager.beginTransaction()
+            .add(R.id.mainViewPager, MyAppointmentFragment()).commit()
     }
 }

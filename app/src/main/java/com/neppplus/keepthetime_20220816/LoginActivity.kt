8c97e25f29b51560abd4bcc79dcf16e975b7575f
넵플러스 <kmc901206@gmail.com>
2022-08-16 09:45:27 +0900

    스플래시 액티비티 생성 > 로그인 액티비티 생성

---
+++ app/src/main/java/com/neppplus/keepthetime_20220816/LoginActivity.kt
@@ -0,0 +1,11 @@
+package com.neppplus.keepthetime_20220816
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}

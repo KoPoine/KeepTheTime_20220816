e11d429f51fe7b718ece8c02fbeaf06388fb361c
넵플러스 <kmc901206@gmail.com>
2022-08-17 12:26:55 +0900

    네이버 지도 기본 코드 작성

--- app/src/main/java/com/neppplus/keepthetime_20220816/AddMyPlaceActivity.kt
+++ app/src/main/java/com/neppplus/keepthetime_20220816/AddMyPlaceActivity.kt
@@ -2,10 +2,12 @@ package com.neppplus.keepthetime_20220816
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.naver.maps.geometry.LatLng
 import com.naver.maps.map.CameraPosition
 import com.naver.maps.map.CameraUpdate
 import com.naver.maps.map.MapFragment
+import com.naver.maps.map.overlay.Marker
 
 class AddMyPlaceActivity : BaseActivity() {
 
@@ -35,13 +37,27 @@ class AddMyPlaceActivity : BaseActivity() {
 
             val coord = LatLng(37.5779235853308, 127.033553463647)
 
+            val latitude = 37.5779235853308
+            val longitude = 127.033553463647
 
-            val cameraPosition = CameraPosition(coord, 20.0)
+            val cameraPosition = CameraPosition(coord, 16.0)
 
 //            coord에 설정한 좌표(기본 좌표 => 학원 좌표값) > 네이버 지도의 카메라 이동
             val cameraUpdate = CameraUpdate.scrollTo(coord)
 //            naverMap.moveCamera(cameraUpdate)
             naverMap.cameraPosition = cameraPosition
+
+//            마커 생성 => 위치 : 기본 좌표(coord)
+            val marker = Marker()
+            marker.position = LatLng(latitude, longitude)
+            marker.map = naverMap
+
+            naverMap.setOnMapClickListener { pointF, latLng ->
+                Log.d("클릭된 위/경도", "위도 : ${latLng.latitude}, 경도 : ${latLng.longitude}")
+
+                marker.position = latLng
+                marker.map = naverMap
+            }
         }
     }
 

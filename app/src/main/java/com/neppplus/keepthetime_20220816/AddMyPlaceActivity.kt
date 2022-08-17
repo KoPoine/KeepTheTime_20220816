25ace0a65906e544c48074d4981810736a75e93b
넵플러스 <kmc901206@gmail.com>
2022-08-17 11:20:56 +0900

    내 출발 장소 등록 액티비티 생성

---
+++ app/src/main/java/com/neppplus/keepthetime_20220816/AddMyPlaceActivity.kt
@@ -0,0 +1,11 @@
+package com.neppplus.keepthetime_20220816
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AddMyPlaceActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_my_place)
+    }
+}

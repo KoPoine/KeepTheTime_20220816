9351d7e799e4c7f5966b32274f171696670ec876
넵플러스 <kmc901206@gmail.com>
2022-08-29 13:52:54 +0900

    커스텀 액션바 장착

--- app/src/main/java/com/neppplus/keepthetime_20220816/BaseActivity.kt
+++ app/src/main/java/com/neppplus/keepthetime_20220816/BaseActivity.kt
@@ -2,7 +2,9 @@ package com.neppplus.keepthetime_20220816
 
 import android.content.Context
 import android.os.Bundle
+import androidx.appcompat.app.ActionBar
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import com.neppplus.keepthetime_20220816.api.APIList
 import com.neppplus.keepthetime_20220816.api.ServerAPI
 import retrofit2.Retrofit
@@ -19,8 +21,20 @@ abstract class BaseActivity : AppCompatActivity() {
         mContext = this
         retrofit = ServerAPI.getRetrofit(mContext)
         apiList = retrofit.create(APIList::class.java)
+
+        supportActionBar?.let {
+            setCustomActionBar()
+        }
     }
 
     abstract fun setupEvents()
     abstract fun setValues()
+
+    fun setCustomActionBar() {
+        val defaultActionBar = supportActionBar!!
+        defaultActionBar.displayOptions = ActionBar.DISPLAY_SHOW_CUSTOM
+        defaultActionBar.setCustomView(R.layout.custom_action_bar)
+        val myToolbar = defaultActionBar.customView.parent as Toolbar
+        myToolbar.setContentInsetsAbsolute(0, 0)
+    }
 }
